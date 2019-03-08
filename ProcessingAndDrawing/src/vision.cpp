9bcf067121cc039dc20f28555457c30788e9e34f
Andrew <agcummings11@gmail.com>
2019-03-08 13:46:56 -0800

    nonworking rodgresguz

--- ProcessingAndDrawing/src/vision.cpp
+++ ProcessingAndDrawing/src/vision.cpp
@@ -18,33 +18,33 @@ int MAX_VAL = 255;
 
 Scalar threshMin = Scalar(MIN_HUE, MIN_SAT, MIN_VAL);
 Scalar threshMax = Scalar(MAX_HUE, MAX_SAT, MAX_VAL);
-float minArea = 300;
-float maxAreaRatio = 8;
-float minDensity = 0.75;
 
-float h = 5.14192; // height in inches of tape that we compare to
+float MIN_DENSITY = 0.75;
+int CONTOURS_TO_CHECK = 5;
+int pos_thresh = 0;
+
+float h = 5.2; //5.14192; //0.50076; // height in inches of tape that we compare to
 float w = 11.739; //  width in inches of target that we compare to
 
 float p = 0.0;   // angle of camera relative to ground // 0.2788 for real robit
 float W = 640.0; // width of each frame
 float H = 480.0; // height of each frame
-float s_x = 63.0/180.0 * PI; // FOV in x axis of camera in radians
+float s_x = 60.0/180.0 * PI; // FOV in x axis of camera in radians
 float s_y = s_x /  W * H; // FOV in y axis of camera  in radians
 
 float iData[9] = {559.57544781, 0.0, 287.88704838, 0.0, 558.25711671, 253.80973506, 0.0, 0.0, 1.0};
 float dData[5] = {-0.0614758, -0.10311039, 0.00629035, -0.00674438, 0.47869622};
-
 Mat intrinsic = Mat(Size(3,3), CV_32F, iData);
 Mat distCoeffs = Mat(Size(1,5), CV_32F, dData);
 
 float a = 14.5 / 180.0 * PI;
-float l = 2.0;
+float l = 5.0;
 
-Point3f topLeft3(-w/2, 0, 0);
-Point3f innerLeft3(-w/2 + l * cos(a), -l *sin(a), 0);
+Point3f topLeft3(-w / 2, 0, 0);
+Point3f outerLeft3(-w/2 - l*sin(a), -l*cos(a), 0);
 Point3f topRight3(w/2, 0, 0);
-Point3f innerRight3(w/2 - l * cos(a), -l *sin(a), 0);
-std::vector<Point3f> objPoints = {topLeft3,innerLeft3,topRight3,innerRight3};
+Point3f outerRight3(w/2 + l*sin(a), -l*cos(a), 0);
+std::vector<Point3f> objPoints = {topLeft3, outerLeft3, topRight3, outerRight3};
 
 VisionResultsPackage last;
 
@@ -60,13 +60,14 @@ VisionResultsPackage calculate(const Mat &bgr){
     ui64 time_began = millis_since_epoch();
     VisionResultsPackage res;
 
-    // blur(bgr, bgr, Size(3, 3));
+    // Mat bgrFixed = Mat(bgr.size(), CV_8UC3);
+    // undistort(bgr, bgrFixed, intrinsic, distCoeffs);
 
     //convert to hsv
     Mat hsvMat;
     cvtColor(bgr, hsvMat, COLOR_BGR2HSV);
 
-    //threshold on green (light ring color), any saturation, high brightness
+    //threshold on green (light ring color), high saturation, high brightness
     Mat threshedImg;
     inRange(hsvMat, threshMin, threshMax, threshedImg);
 
@@ -83,82 +84,113 @@ VisionResultsPackage calculate(const Mat &bgr){
     // sort by size in descending order
     std::sort(contours.begin(), contours.end(), sortContour);
 
-    // find 2 biggest contours with area > minArea
+    // go thru the top CONTOURS_TO_CHECK contours, or as many as possible if we don't have that many
     int contoursSize = static_cast<int>(contours.size());
-
-    RotatedRect cont1;
-    float cont1Area = 0;
-    bool found1 = false;
-    RotatedRect cont2;
-    bool found2 = false;
-    for (int i = 0; i < contoursSize; i++) {
+    // cout << "n contours  " << contoursSize << endl;
+    int numContours = std::min(CONTOURS_TO_CHECK, contoursSize);
+    contour_type cont1;
+    bool foundRect1 = false;
+    contour_type cont2;
+    bool foundRect2 = false;
+    for (int i = 0; i < numContours; i++) {
         contour_type cont = contours[i];
-        float totalArea = contourArea(cont, FALSE);
+        double totalArea = contourArea(cont, FALSE);
+        // cout << "contour " << i << " area = " << totalArea << endl;
         RotatedRect rect = minAreaRect(cont);
-        float hullArea = rect.size.width * rect.size.height;
-        float density = totalArea / hullArea;
-        if (totalArea > minArea && density > minDensity) {
-            if (!found1) {
-                found1 = true;
-                cont1 = rect;
-                cont1Area = totalArea;
-            } else if (!found2 && cont1Area / totalArea < maxAreaRatio) {
-                found2 = true;
-                cont2 = rect;
-                i = contoursSize;
+        double rectArea = rect.size.width * rect.size.height;
+        double density = totalArea / rectArea; // compare area of the contour to the area of its bounding rect
+        if (density >= MIN_DENSITY) {
+            if (!foundRect1) {
+                foundRect1 = true;
+                cont1 = cont;
+            } else {
+                foundRect2 = true;
+                cont2 = cont;
+                i = numContours; // if we found both contours we're done
             }
-        } else if (totalArea < minArea) {
-            i = contoursSize;
         }
     }
 
-    if (!found1 || !found2) { //  if we ended without both contours we exit
+    if (!foundRect1 || !foundRect2) { //  if we ended without both contours we exit
         return res; // with failure result
     }
 
-    float theta = ((cont1.center.x + cont2.center.x) / 2.0) * s_x / W;
+    Moments m1 = moments(cont1);
+    Moments m2 = moments(cont2);
+    float cx1 = m1.m10 / m1.m00;
+    float cx2 = m2.m10 / m2.m00;
+    float thetaPixels = (cx1 + cx2)/2.0 - W / 2;
+    float theta = thetaPixels * s_x / W;
 
-    Point2f leftRect[4];
-    Point2f rightRect[4];
-    if (cont1.center.x < cont2.center.x)
-    {
-        cont1.points(leftRect);
-        cont2.points(rightRect);
-    }
-    else
-    {
-        cont2.points(leftRect);
-        cont1.points(rightRect);
-    }
-    float leftX[4];
-    float leftY[4];
-    float rightX[4];
-    float rightY[4];
-    for (int i = 0; i < 4; i++)
-    {
-        leftX[i] = leftRect[i].x;
-        leftY[i] = leftRect[i].y;
-        rightX[i] = rightRect[i].x;
-        rightY[i] = rightRect[i].y;
+    contour_type leftPoints;
+    contour_type rightPoints;
+
+    if (cx1 < cx2) {
+        leftPoints = cont1;
+        rightPoints = cont2;
+        // approxPolyDP(cont1, leftPoints, 2, true);
+        // approxPolyDP(cont2, rightPoints, 2, true);
+    } else {
+        leftPoints = cont2;
+        rightPoints = cont1;
+        // approxPolyDP(cont2, leftPoints, 2, true);
+        // approxPolyDP(cont1, rightPoints, 2, true);
     }
 
-    int leftMinY = distance(leftY, min_element(leftY, leftY + 4));
-    int rightMinY = distance(rightY, min_element(rightY, rightY + 4));
-    int leftMaxX = distance(leftX, max_element(leftX, leftX + 4));
-    int rightMinX = distance(rightX, min_element(rightX, rightX + 4));
+    int leftSize = static_cast<int>(leftPoints.size());
+    int rightSize = static_cast<int>(rightPoints.size());
 
-    Point2f topLeft = leftRect[leftMinY];
-    Point2f innerLeft = leftRect[leftMaxX];
-    Point2f topRight = rightRect[rightMinY];
-    Point2f innerRight = rightRect[rightMinX];
+    float leftX[leftSize];
+    float leftY[leftSize];
+    float rightX[rightSize];
+    float rightY[rightSize];
+    for (int i = 0; i < leftSize; i++) {
+        leftX[i] = leftPoints[i].x;
+        leftY[i] = leftPoints[i].y;
+    }
+    for (int i = 0; i < rightSize; i++)
+    {
+        rightX[i] = rightPoints[i].x;
+        rightY[i] = rightPoints[i].y;
+    }
 
-    Mat processedImage = threshedImg.clone();
-    threshold(processedImage, processedImage, 0, 255, THRESH_BINARY);
-    circle(processedImage, topLeft, 2, Scalar(190, 190, 190), 2);
-    circle(processedImage, innerLeft, 2, Scalar(190, 190, 190), 2);
-    circle(processedImage, topRight, 2, Scalar(190, 190, 190), 2);
-    circle(processedImage, innerRight, 2, Scalar(190, 190, 190), 2);
-    cvtColor(processedImage, processedImage, CV_GRAY2BGR);
+    int leftMinY = leftY[distance(leftY, min_element(leftY, leftY+leftSize))] + pos_thresh;
+    int rightMinY = rightY[distance(rightY, min_element(rightY, rightY+rightSize))] + pos_thresh;
+    int leftMinX = leftX[distance(leftX, min_element(leftX, leftX+leftSize))] + pos_thresh;
+    int rightMaxX = rightX[distance(rightX, max_element(rightX, rightX+rightSize))] - pos_thresh;
+
+    Point2f topLeft(W, 0);
+    Point2f outerLeft(0, 0);
+    for (int i = 0; i < leftSize; i++) {
+        Point2f p = leftPoints[i];
+        if (p.y <= leftMinY && p.x < topLeft.x) {
+            topLeft = p;
+        }
+        if (p.x <= leftMinX && p.y > outerLeft.y) {
+            outerLeft = p;
+        }
+    }
+
+    Point2f topRight(0, 0);
+    Point2f outerRight(0, 0);
+    for (int i = 0; i < rightSize; i++)
+    {
+        Point2f p = rightPoints[i];
+        if (p.y <= rightMinY && p.x > topRight.x)
+        {
+            topRight = p;
+        }
+        if (p.x >= rightMaxX && p.y > outerRight.y)
+        {
+            outerRight = p;
+        }
+    }
+
+    Mat processedImage = bgr.clone();
+    processedImage.at<Vec3b>(topLeft) = Vec3b(0, 255, 0);
+    processedImage.at<Vec3b>(outerLeft) = Vec3b(0, 255, 0);
+    processedImage.at<Vec3b>(topRight) = Vec3b(0, 255, 0);
+    processedImage.at<Vec3b>(outerRight) = Vec3b(0, 255, 0);
     if (imgCount == 30)
     {
         vector<int> compression_params;
@@ -172,7 +204,7 @@ VisionResultsPackage calculate(const Mat &bgr){
         imgCount++;
     }
 
-    std::vector<Point2f> imgPoints = {topLeft, innerLeft, topRight, innerRight};
+    std::vector<Point2f> imgPoints = {topLeft, outerLeft, topRight, outerRight};
     solvePnP(objPoints, imgPoints, intrinsic, distCoeffs, last.rvec, last.tvec, true, CV_P3P);
     Mat R(3,3,CV_64F);
     Rodrigues(last.rvec, R);
@@ -181,7 +213,7 @@ VisionResultsPackage calculate(const Mat &bgr){
     Point3d posPoint(pos);
 
     // cout << last.rvec << endl;
-    cout << last.tvec << endl;
+    // cout << last.tvec << endl;
 
     //create the results package
     res.valid = true;
@@ -190,7 +222,7 @@ VisionResultsPackage calculate(const Mat &bgr){
     res.robotPos.x = posPoint.x;
     res.robotPos.y = posPoint.z;
 
-    // cout << posPoint << endl;
+    cout << posPoint << endl;
     // cout << res.robotPos.x << ", " << res.robotPos.y << ", " << theta << endl;
 
     return res;
