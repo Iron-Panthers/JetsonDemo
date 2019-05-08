aeaff0d707a35c77ae75ae79ca0dcd3d540791a5
Andrew <agcummings11@gmail.com>
2019-05-08 15:26:53 -0700

    maybe the vector was the problem

--- ProcessingAndDrawing/src/vision.cpp
+++ ProcessingAndDrawing/src/vision.cpp
@@ -41,19 +41,19 @@ vector<contour_type> findContours(const Mat &bgr, Scalar minHSV, Scalar maxHSV, 
     // sort by area in descending order
     std::sort(contours.begin(), contours.end(), sortContour);
 
-    // remove any vectors with invalid density
-    for (int i = static_cast<int>(contours.size()); i >= 0; i--) // decrement for no conflict
-    {
-        contour_type cont = contours[i];
-        double totalArea = contourArea(cont, FALSE);
-        RotatedRect rect = minAreaRect(cont);
-        double rectArea = rect.size.width * rect.size.height;
-        double density = totalArea / rectArea; // compare area of the contour to the area of its bounding rect
-        if (density < minDensity)
-        {
-            contours.erase(contours.begin() + i);
-        }
-    }
+    // // remove any vectors with invalid density
+    // for (int i = static_cast<int>(contours.size()); i >= 0; i--) // decrement for no conflict
+    // {
+    //     contour_type cont = contours[i];
+    //     double totalArea = contourArea(cont, FALSE);
+    //     RotatedRect rect = minAreaRect(cont);
+    //     double rectArea = rect.size.width * rect.size.height;
+    //     double density = totalArea / rectArea; // compare area of the contour to the area of its bounding rect
+    //     if (density < minDensity)
+    //     {
+    //         contours.erase(contours.begin() + i);
+    //     }
+    // }
 
     return contours;
 }
@@ -64,37 +64,7 @@ VisionResultsPackage calculate(const Mat &bgr)
     ui64 time_began = millis_since_epoch();
     VisionResultsPackage res;
 
-    //convert to hsv
-    Mat hsvMat;
-    cvtColor(bgr, hsvMat, COLOR_BGR2HSV);
-
-    //threshold
-    Mat threshedImg;
-    inRange(hsvMat, cargoMinHSV, cargoMaxHSV, threshedImg);
-
-    //find contours
-    vector<contour_type> cargo;
-    vector<Vec4i> hierarchy; //throwaway, needed for function
-    findContours(threshedImg, cargo, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-
-    // sort by area in descending order
-    std::sort(cargo.begin(), cargo.end(), sortContour);
-
-    // remove any vectors with invalid density
-    for (int i = static_cast<int>(cargo.size()); i >= 0; i--) // decrement for no conflict
-    {
-        contour_type cont = cargo[i];
-        double totalArea = contourArea(cont, FALSE);
-        RotatedRect rect = minAreaRect(cont);
-        double rectArea = rect.size.width * rect.size.height;
-        double density = totalArea / rectArea; // compare area of the contour to the area of its bounding rect
-        if (density < cargoMinDensity)
-        {
-            cargo.erase(cargo.begin() + i);
-        }
-    }
-
-    // vector<contour_type> cargo = findContours(bgr, cargoMinHSV, cargoMaxHSV, cargoMinDensity);
+    vector<contour_type> cargo = findContours(bgr, cargoMinHSV, cargoMaxHSV, cargoMinDensity);
 
     if (static_cast<int>(cargo.size()) == 0)
     {
